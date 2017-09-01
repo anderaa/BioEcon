fe4641bb34b4c8674b1d7306174a7a8fd5d4d363
anderaa <aaronanderson.co@gmail.com>
2017-09-01 09:30:21 -0600

    updated about page with license and version

--- Shiny/ui.R
+++ Shiny/ui.R
@@ -24,7 +24,7 @@ library(shiny)
 # Define UI for application:
 shinyUI(fluidPage(tags$link(rel = 'stylesheet', type = 'text/css', href = 'styles.css'),
   img(src = "logo.png", height = 47, align='left'),      
-  titlePanel(strong('BioEcon for Canine Rabies'), windowTitle='BioEcon'), 
+  titlePanel(strong('BioEcon for Canine Rabies - v0.1'), windowTitle='BioEcon'), 
     tabsetPanel(id='mainTabs', type='pills',
       tabPanel('Model Setup',
         navlistPanel('Model Setup', widths=c(3, 9),
@@ -294,11 +294,36 @@ shinyUI(fluidPage(tags$link(rel = 'stylesheet', type = 'text/css', href = 'style
         br(), br(),
         'Built by Aaron Anderson, Johann Kotze, Brody Hatch, and Jordan Navin',
         br(), br(),
-        'Inquiries to: Aaron.M.Anderson@aphis.usda.gov', 
+        'Version 0.1. Last updated September 1, 2017.',
+        br(), br(),
+        'Inquiries and bugs to: Aaron.M.Anderson@aphis.usda.gov', 
         br(), br(),
         'Custom builds available.', 
         br(), br(),
-        img(src = "nwrcLogo.png", height = 75, align='left')
+        img(src = "nwrcLogo.png", height = 75, align='left'),
+        br(), br(), br(), br(), br(),
+        'MIT License',
+        br(),
+        'Copyright (c) 2017 Aaron Anderson',
+        br(),
+        'Permission is hereby granted, free of charge, to any person obtaining a copy
+        of this software and associated documentation files (the "Software"), to deal
+        in the Software without restriction, including without limitation the rights
+        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+        copies of the Software, and to permit persons to whom the Software is
+        furnished to do so, subject to the following conditions:',
+        br(),
+        'The above copyright notice and this permission notice shall be included in all
+        copies or substantial portions of the Software.',
+        br(),
+        'THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+        SOFTWARE.'
+        
       )  # close Help tabPanel
     )  # close mainTabs tabsetPanel
 )  # close fluidPage
